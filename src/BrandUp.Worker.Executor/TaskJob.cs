a98d0772b86f11a562f36f557b5f70d83b8761a6
Dmitry <d.kovyazin@hotmail.com>
2019-04-29 20:07:00 +0700

    Change log level on task execution timeout.

--- src/BrandUp.Worker.Executor/TaskJob.cs
+++ src/BrandUp.Worker.Executor/TaskJob.cs
@@ -66,7 +66,7 @@ namespace BrandUp.Worker.Executor
 
                     if (executionWatch.ElapsedMilliseconds >= timeoutInMilliseconds)
                     {
-                        logger.LogInformation($"Task {TaskId} execution timeout.");
+                        logger.LogWarning($"Task {TaskId} execution timeout.");
 
                         await executorContext.OnTimeoutJob(this);
                     }
